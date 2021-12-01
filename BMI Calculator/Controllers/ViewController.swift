ea42af95a0abb60d126e065f9dce9f7bd43f400f
Ryan A. Anderson <ryan.anderson9921@stu.mtec.edu>
2021-12-01 08:30:19 -0700

    Connecting sliders
    Trouble shooting problems

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -14,7 +14,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let currentValue = String(format: "%.2f", sender.value)
+        print(currentValue)
+    }
+    @IBAction func weighSliderChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
 }
 
